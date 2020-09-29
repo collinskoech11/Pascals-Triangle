9e323683851056d27b65728b7a8f4bc69fb07edf
Collins Koech <collinskoechck34@gmail.com>
2020-09-29 12:09:10 +0300

    update

---
+++ manage.py
@@ -0,0 +1,29 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+    
+        triangle = []
+
+        if numRows < 1:
+            return triangle
+
+        trangle.append([1])
+
+        i = 1
+        while i < numRows:
+            row = []
+
+            row.append([1])
+
+            x = 1
+
+            while x <= i-1:
+
+                row.append(triangle[i-1][x-1]+triangle[i-1][x])
+                x += 1
+
+            row.append(1)
+
+            triangle.append(row)
+            i+=1
+
+        return triangle
